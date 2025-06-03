09829c4e690a275d1a99d11b0861a0033530901a
Christina Gordon <crissyg@users.noreply.github.com>
2025-06-03 14:32:42 -0400

    Initial Commit

---
+++ PulseTrack/PulseTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseTrackApp.swift
+//  PulseTrack
+//
+//  Created by Tina G on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
